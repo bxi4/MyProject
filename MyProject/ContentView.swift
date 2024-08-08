381d9ae823b1eaaf93e1796d62cede69fb39b9bd
Hmdo <muhammedaahaj@gmail.com>
2024-08-08 13:26:16 +0300

    New Test in Telegram

--- MyProject/ContentView.swift
+++ MyProject/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("Text New Project")
+            Text("Seccond Text")
         }
         .padding()
     }
