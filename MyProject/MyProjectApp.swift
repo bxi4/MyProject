7e0f6fc870ee1e0a4952764f03cc399e66ae32d7
Hmdo <muhammedaahaj@gmail.com>
2024-08-08 13:03:02 +0300

    Initial Commit

---
+++ MyProject/MyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyProjectApp.swift
+//  MyProject
+//
+//  Created by Muhammed Ahaj on 08/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
