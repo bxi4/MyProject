11c5cfabd8c8c828c91072c20c881e453cdf02f3
Hmdo <muhammedaahaj@gmail.com>
2024-08-10 14:17:21 +0300

    Create Check Internet Model

---
+++ MyProject/CheckInternetModel.swift
@@ -0,0 +1,8 @@
+//
+//  CheckInternetModel.swift
+//  MyProject
+//
+//  Created by Muhammed Ahaj on 10/08/2024.
+//
+
+import Foundation
---
+++ MyProject/FlipCardImage.swift
@@ -0,0 +1,18 @@
+//
+//  FlipCardImage.swift
+//  MyProject
+//
+//  Created by Muhammed Ahaj on 08/08/2024.
+//
+
+import SwiftUI
+
+struct FlipCardImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlipCardImage()
+}
